03294d3652b69d7afea56275779e427c8054fb75
oporpino <dev@oporpino.com>
2026-04-21 14:53:01 -0300

    style: add blank line before help hint

--- internal/cmd/init.go
+++ internal/cmd/init.go
@@ -108,7 +108,7 @@ func NewInitCmd() *cobra.Command {
 						clrGray, argW, r.args, clrReset,
 						r.desc)
 				}
-				fmt.Printf("  %srun ward --help for all commands%s\n\n", clrGray, clrReset)
+				fmt.Printf("\n  %srun ward --help for all commands%s\n\n", clrGray, clrReset)
 			}
 		},
 	}
